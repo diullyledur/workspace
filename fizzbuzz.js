246696bc4f63298fc18b78592cd92d7691d6c3ca
diullyledur <diullyledur@gmail.com>
2022-08-23 18:47:57 +0000

    oi

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+function fizzbuzz (numero){
+    let mensagem = ""
+    if (numero % 3 === 0 ){
+        mensagem +="fizz"
+    }
+    if{ (numero % 5 === 0){
+    mensagem += "BUZZ"
+    
+    }
+    if (mensagem.length === 0) { 
+        mensagem = String(numero)
+    }
+    return mensagem
+    }
+    for(let i =0; i <=20;   i++){
+    const mensagem = fizzbuzz (i)
+    console.log (mensagem)
+    
+    }
+    module.exports.fizzbuzz = fizzbuzz 
+    
+    
+    
+    } 
